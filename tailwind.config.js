2cbbee249d2485ecaa5a89e86f40ccc02a2425d7
Joel Kapp <joel.g.kapp@gmail.com>
2021-01-31 20:43:06 -0700

    Extend background Image and z index config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'front-page': "url('/LydiaImg2.jpg')",
+      }),
+      zIndex: {
+        '-10': '-10',
+      },
+    },
   },
   variants: {
     extend: {},
